87185953b90325d45b1a63d7dbfa1ec48bf1aecd
niko <dimiropulosniko@gmail.com>
2022-05-10 20:18:25 +0500

    added again 1st homework

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+var age = +prompt('Введите свой возраст')
+if (age <= 18) {
+   alert('Вы ещё молоды вам надо учиться') 
+}
+else if(age > 18 && age <= 50){
+    alert('Вам надо работать')
+}
+else if (age > 50 && age <= 59) {
+    alert('Вам скоро на пенсию')
+}
+else if (age > 59 && age <= 150) {
+    alert('Вы пенсионер')
+}
+else{
+    alert('Ой что-то пошло не так')
+}
+
 var clock = prompt('Введите время')
 if (clock >= 4 && clock <= 11) {
     alert(`Сейчас ${clock} часов утра`);
